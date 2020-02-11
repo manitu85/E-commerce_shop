b3d09061d41ccad161c7471850adcd6be3bf9681
manitu85 <manitu.psy@gmail.com>
2020-02-11 21:14:38 +0100

    #

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -14,7 +14,7 @@ const CollectionPreview = ({title, items }) => {
     ))
 
   return (
-    <div c lassName='collection-preview'>
+    <div className='collection-preview'>
       <h1 className='title' >{title.toUpperCase()}</h1>
       <div className='preview' >
         { previewItems }
