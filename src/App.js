992d5adecd96c11db7ea432aa9cc518b0a046410
manitu85 <manitu.psy@gmail.com>
2020-02-11 01:43:48 +0100

    #

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 
-import Header from './components/Header/header.component'
+import Header from './components/header/header.component'
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
