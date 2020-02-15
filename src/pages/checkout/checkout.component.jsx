d57d0d491fb3d352a365f5a341e5797147a561eb
manitu85 <manitu.psy@gmail.com>
2020-02-15 13:02:53 +0100

    Completed add-item and remove-item functionality to checkout item component

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -31,7 +31,7 @@ const CheckoutPage = ({cartItems, total}) => (
       )
     }
     <div className='total'>
-      <span> TOTAL: ${total} </span>
+      <span> TOTAL: ${total.toFixed(2)} </span>
     </div>
   </div> 
 )
--- src/redux/cart/cart.action.js
+++ src/redux/cart/cart.action.js
@@ -1,4 +1,10 @@
-import { TOGGLE_CART_HIDDEN, ADD_ITEM } from "./cart.types"
+import { 
+  TOGGLE_CART_HIDDEN, 
+  ADD_ITEM, 
+  CLEAR_ITEM_FROM_CART, 
+  REMOVE_ITEM 
+} from './cart.types'
+
 
 export const toogleCartHidden = () => ({
   type: TOGGLE_CART_HIDDEN
@@ -9,4 +15,13 @@ export const addItem = item => ({
   payload: item
 })
 
+export const removeItem = item => ({
+  type: REMOVE_ITEM,
+  payload: item
+})
+
+export const clearItemFromCart = item => ({
+  type: CLEAR_ITEM_FROM_CART,
+  payload: item
+})
 
