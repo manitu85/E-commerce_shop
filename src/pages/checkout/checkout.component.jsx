4c37ddae891659e794d7065e50aff2bd0df6705a
manitu85 <manitu.psy@gmail.com>
2020-02-16 23:29:50 +0100

    #

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
-import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors'
 
-import './checkout.styles.scss'
-import CheckoutItem from '../../components/checkout-item/checkout-item.component'
+import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors'
 import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component'
+import CheckoutItem from '../../components/checkout-item/checkout-item.component'
+import './checkout.styles.scss'
 
 const CheckoutPage = ({cartItems, total}) => (
   <div className='checkout-page' >
