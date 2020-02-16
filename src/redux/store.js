961a9b8e497ff5af97b1944c6617e7fa903a39f6
manitu85 <manitu.psy@gmail.com>
2020-02-17 00:37:26 +0100

    #

--- src/redux/store.js
+++ src/redux/store.js
@@ -7,7 +7,10 @@ import  { persistStore } from 'redux-persist'
 const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose
 
 // Middlewares
-const middlewares = [logger]
+const middlewares = []
+if (process.env.NODE_ENV === 'development'){
+  middlewares.push(logger)
+}
 
 // Create store
 const store = createStore(
