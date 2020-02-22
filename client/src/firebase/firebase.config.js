1c2902ad51b06d34d35535df4a24883242d4419f
manitu85 <manitu.psy@gmail.com>
2020-02-22 21:17:40 +0100

    #

---
+++ client/src/firebase/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCt2CFTGE9vcdxFKi-udUg5exCspevGiUI",
+  authDomain: "crnw-shop-db4532.firebaseapp.com",
+  databaseURL: "https://crnw-shop-db4532.firebaseio.com",
+  projectId: "crnw-shop-db4532",
+  storageBucket: "crnw-shop-db4532.appspot.com",
+  messagingSenderId: "563145129629",
+  appId: "1:563145129629:web:03923eeb398247c92bde74"
+}
+
+export default firebaseConfig
