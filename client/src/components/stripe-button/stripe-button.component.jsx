d6112dc84e12eddd31400a62b9af8124d305d9c6
manitu85 <manitu.psy@gmail.com>
2020-02-22 19:53:25 +0100

    Add express server, stripe route on both sides

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import StripeCheckout from 'react-stripe-checkout'
+import axios from 'axios'
 
 
 const StripeCheckoutButton = ({ price }) => {
@@ -7,8 +8,22 @@ const StripeCheckoutButton = ({ price }) => {
   const publishableKey = 'pk_test_D2n4Owmb51oapFkSRPoTUR9X00Cq1hmxBA'
 
   const onToken = token => {
-    console.log(token);
-    alert("Payment Successful")
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    })
+    .then(res => {
+      alert('Payment Successful')
+    }).catch(error => {
+      console.log('Payment error: ', JSON.parse(error))
+      alert('There was an issue with your payment. Please sure you use the provided credit cart.')
+    })
+
+
   }
 
   return (
