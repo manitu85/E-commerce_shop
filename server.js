04d1cefbedf9cbefa31d5114e4d1fed55d0fb401
manitu85 <manitu.psy@gmail.com>
2020-02-27 18:38:35 +0100

    #

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // app.use(compression());
-// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(enforce.HTTPS({ trustProtoHeader: true })); 
 app.use(bodyParser.json());
-app.use(enforce.HTTPS({ trustProtoHeader: true })); 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
